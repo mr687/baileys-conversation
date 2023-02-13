137607e4fbd71ee5788353f4186de96275c93d60
mr687 <dapino.dev@gmail.com>
2023-02-14 02:19:39 +0700

    setup conversation package

--- README.md
+++ README.md
@@ -1,44 +1,46 @@
-# Project Name
+# baileys conversation
 
-[![Build Status](https://travis-ci.org/username/project.svg?branch=master)](https://travis-ci.org/username/project)
-[![Coverage Status](https://coveralls.io/repos/github/username/project/badge.svg?branch=master)](https://coveralls.io/github/username/project?branch=master)
-[![Dependency Status](https://david-dm.org/username/project.svg)](https://david-dm.org/username/project)
-[![devDependency Status](https://david-dm.org/username/project/dev-status.svg)](https://david-dm.org/username/project#info=devDependencies)
-[![npm version](https://badge.fury.io/js/project.svg)](https://badge.fury.io/js/project)
 
 ## Description
 
-Project description
+This package is for stores baileys conversation to MongoDB.
 
 ## Installation
 
 ```bash
-npm install project
+yarn add baileys-conversation
 ```
 
 ## Usage
 
-```js
-var project = require('project');
+```ts
+import { useMongoConversation } from 'baileys-conversation'
+import mongoose from 'mongoose'
+import makeWASocket from '@adiwajshing/baileys'
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI)
+
+  const connection = mongoose.connection
+  await startSocket(connection)
+}
+
+async function startSocket(connection) {
+  const conversation = useMongoConversation(connection, {
+    ignoreFromMe: false,
+    ignoreJids: [
+      // '6282316512341@s.whatsapp.net'
+    ],
+    ignoreMessage: (message) => false,
+  })
+
+  const sock = makeWASocket({})
+  conversation.binding(socket)
+  
+  // ...
+  
+  return sock
+}
+
+main()
 ```
-
-## API
-
-### project()
-
-Description
-
-## License
-
-MIT © [Your Name](http://your-website.com)
-
-[travis-image]: https://travis-ci.org/username/project.svg?branch=master
-[travis-url]: https://travis-ci.org/username/project
-[coveralls-image]: https://coveralls.io/repos/github/username/project/badge.svg?branch=master
-[coveralls-url]: https://coveralls.io/github/username/project?branch=master
-[david-image]: https://david-dm.org/username/project.svg
-[david-url]: https://david-dm.org/username/project
-[david-dev-image]: https://david-dm.org/username/project/dev-status.svg
-[david-dev-url]: https://david-dm.org/username/project#info=devDependencies
-[npm-image]: https://badge.fury.io/js/project.svg
-[npm-url]: https://badge.fury.io/js/project
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,157 @@
-console.log('Hello world')
+import { BaileysEventMap, WASocket, jidNormalizedUser, proto } from '@adiwajshing/baileys'
+import { Connection, Schema } from 'mongoose'
+
+export enum ConversationType {
+  TEXT = 'text',
+}
+
+export type ConversationMessageText = {
+  id: string
+  type: ConversationType.TEXT
+  content: string
+  timestamp: number
+}
+
+export type ConversationMessage = ConversationMessageText
+
+export type Conversation = {
+  recipientId: string
+  senderId: string
+  messages: ConversationMessage[]
+}
+
+const ConversationSchema = new Schema({
+  recipientId: {
+    type: String,
+    required: true,
+  },
+  senderId: {
+    type: String,
+    required: true,
+  },
+  messages: {
+    type: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+        type: {
+          type: String,
+          required: true,
+        },
+        content: {
+          type: String,
+          required: true,
+        },
+        timestamp: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+    default: [],
+  },
+})
+
+export type ConversationOptions = {
+  /** List of JIDs to blacklist, so that the conversation will not be saved */
+  ignoreJids: string[]
+  /** Ignore messages from me */
+  ignoreFromMe: boolean
+  /** Ignore messages that match this function */
+  ignoreMessage: (message: proto.IWebMessageInfo) => boolean
+}
+
+export const useMongoConversation = (connection: Connection, options?: Partial<ConversationOptions>) => {
+  let socket: WASocket | undefined
+  const { ignoreJids = [], ignoreFromMe = false, ignoreMessage = (_: proto.IWebMessageInfo) => false } = options || {}
+
+  const model = connection.models.Conversation || connection.model<Conversation>('Conversation', ConversationSchema)
+
+  const saveData = async (data: Conversation) => {
+    const { messages } = data
+    if (!messages.length) {
+      return
+    }
+
+    const saveOptions = { upsert: true }
+    const saveCondition: Partial<Conversation> = {
+      recipientId: data.recipientId,
+      senderId: data.senderId,
+    }
+    const saveValue = {
+      $push: {
+        messages: messages[0],
+      },
+    }
+    return model.updateOne(saveCondition, saveValue, saveOptions)
+  }
+
+  const mapConversation = (message: proto.IWebMessageInfo): Conversation => {
+    const { key, message: messageContent } = message
+    const { remoteJid } = key
+
+    let content = ''
+    // only save message with type conversation (text only)
+    if (messageContent?.conversation) {
+      content = messageContent.conversation
+    } else if (messageContent?.extendedTextMessage) {
+      content = messageContent.extendedTextMessage.text || ''
+    }
+
+    const messageToBeSaved: ConversationMessageText = {
+      id: message.key.id || '',
+      type: ConversationType.TEXT,
+      content: content,
+      timestamp: +(message.messageTimestamp || 0),
+    }
+
+    return {
+      recipientId: jidNormalizedUser(remoteJid!),
+      senderId: socket!.user!.id,
+      messages: content ? [messageToBeSaved] : [],
+    }
+  }
+
+  const messageUpsertHandler = async (upsert: BaileysEventMap['messages.upsert']) => {
+    const { messages, type } = upsert
+    const tasks: any[] = []
+
+    if (!['append', 'notify'].includes(type)) {
+      return
+    }
+
+    for (const message of messages) {
+      const jid = jidNormalizedUser(message.key.remoteJid!)
+      if (ignoreJids.includes(jid)) {
+        continue
+      }
+      if (ignoreFromMe && message.key.fromMe) {
+        continue
+      }
+      if (ignoreMessage(message)) {
+        continue
+      }
+
+      const conversation = mapConversation(message)
+      tasks.push(saveData(conversation))
+    }
+
+    await Promise.all(tasks)
+  }
+
+  const handler = (target: any) => {
+    socket = target
+    socket!.ev.process(async events => {
+      if (events['messages.upsert']) {
+        await messageUpsertHandler(events['messages.upsert'])
+      }
+    })
+  }
+
+  return {
+    binding: handler,
+  }
+}
